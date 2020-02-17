7e26712db1f39bb7faf24338e19e5077693d9dea
Student <student@Makerss-MacBook-Air.local>
2020-02-17 15:43:43 +0000

    return the number

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -14,4 +14,7 @@ describe('fizzBuzz', function() {
     it('returns fizz if multiple of 3', function() {
         expect(fizzBuzz(9)).toBe('Fizz');
     });
+    it('returns the number if is not divible by 3 or 5', function(){
+        expect(fizzBuzz(7)).toBe(7)
+    });
 });
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,9 +1,9 @@
 function fizzBuzz(num) {
-    if (num === 3){
+    if (num === 15 ) {
+        return 'FizzBuzz'    
+    } else if (num % 3 == 0){
         return 'Fizz'
-    } else if ( num === 5) {
+    } else if ( num % 5 == 0) {
         return 'Buzz'
-    } else {
-        return 'FizzBuzz' 
-    };  
+     };  
 };
